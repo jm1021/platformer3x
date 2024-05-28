669308252fd69695283246e5c19e68af90edd72f
Aiden Huynh <ah5993909@gmail.com>
2024-05-28 14:05:06 -0700

    Fix resizing on hills level

--- assets/js/platformer3x/JumpPlatform.js
+++ assets/js/platformer3x/JumpPlatform.js
@@ -5,13 +5,13 @@ import GameObject from './GameObject.js';
 export class JumpPlatform extends GameObject {
     constructor(canvas, image, data, xPercentage, yPercentage, name) {
         super(canvas, image, data);
+        this.xPercentage = xPercentage
+        this.yPercentage = yPercentage
         this.platformX = xPercentage * GameEnv.innerWidth;
         this.platformY = yPercentage;
         this.data = data;
         this.name = name;
         this.relativeX = ""; //used for the item block's spritesheet.
-
-
 
     }
 
@@ -40,26 +40,23 @@ export class JumpPlatform extends GameObject {
     
     // Set platform position
     size() {
-        // Formula for Height should be on constant ratio, using a proportion of 832
-        const scaledHeight = GameEnv.innerHeight * (this.data.sizeRatio / 832);
-        const scaledWidth = GameEnv.innerHeight * .1;  // width of jump platform is 1/10 of height
-        const platformX = this.platformX
-        const platformY = (GameEnv.bottom - scaledHeight) * this.platformY;
-        this.x = platformX
-        this.y = platformY
 
+        // Formula for Height should be on constant ratio, using a proportion of 832
+        const scaledHeight = GameEnv.innerWidth * (1/27);
+        const scaledWidth = scaledHeight;  // width of jump platform is 1/10 of height
+        const platformX = this.xPercentage * GameEnv.innerWidth
+        const platformY = (GameEnv.bottom - scaledHeight) * this.platformY;
         // set variables used in Display and Collision algorithms
         this.bottom = platformY;
         this.collisionHeight = scaledHeight;
         this.collisionWidth = scaledWidth;
-    
-        //this.canvas.width = this.width; 
+        //this.canvas.width = this.width;
         //this.canvas.height = this.height;
         this.canvas.style.width = `${scaledWidth}px`;
         this.canvas.style.height = `${scaledHeight}px`;
         this.canvas.style.position = 'absolute';
         this.canvas.style.left = `${platformX}px`;
-        this.canvas.style.bottom = `${platformY}px`; 
+        this.canvas.style.top = `${platformY}px`;
     }
 
     // Draw position is always 0,0
